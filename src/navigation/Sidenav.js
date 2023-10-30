f09093d1217b54f58b793aba7a5781ce97cdf63d
Nurdan Emin <nurdanemin0@gmail.com>
2023-10-31 00:17:14 +0300

    Sidebar Navigation

--- src/navigation/Sidenav.js
+++ src/navigation/Sidenav.js
@@ -1,10 +1,59 @@
 import React from "react";
 import "./Sidenav.css";
+import HomeIcon from '@mui/icons-material/Home';
+import SearchIcon from '@mui/icons-material/Search';
+import ExploreIcon from '@mui/icons-material/Explore';
+import SlideshowIcon from '@mui/icons-material/Slideshow';
+import MessageIcon from '@mui/icons-material/Message';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import AddBoxIcon from '@mui/icons-material/AddBox';
+import MenuIcon from '@mui/icons-material/Menu';
 
 function Sidenav() {
     return (
-        <div>
-            Sidenav
+        <div className="sidenav">
+            <img
+                className="sidenav__logo"
+                src="https://www.pngkey.com/png/full/828-8286178_mackeys-work-needs-no-elaborate-presentation-or-distracting.png"
+                alt="Instagram Logo"
+            />
+            <div className="sidenav__buttons">
+                <button
+                    className="side__button">
+                    <HomeIcon/>
+                    <span>Home</span>
+                </button>
+                <button className="side__button">
+                    <SearchIcon/>
+                    <span>Search</span>
+                </button>
+                <button className="side__button">
+                    <ExploreIcon/>
+                    <span>Explore</span>
+                </button>
+                <button className="side__button">
+                    <SlideshowIcon/>
+                    <span>Reels</span>
+                </button>
+                <button className="side__button">
+                    <MessageIcon/>
+                    <span>Messages</span>
+                </button>
+                <button className="side__button">
+                    <FavoriteBorderIcon/>
+                    <span>Notifications</span>
+                </button>
+                <button className="side__button">
+                    <AddBoxIcon/>
+                    <span>Create</span>
+                </button>
+            </div>
+            <div className="sidenav__more">
+                <button className="side__button">
+                    <MenuIcon/>
+                    <span> More</span>
+                </button>
+            </div>
         </div>
     )
 }
